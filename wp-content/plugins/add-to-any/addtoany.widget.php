735f106154a5a95abf8f8c812a21054c1453bad2
Jason Adam Young <jayoung@extension.org>
2014-08-06 16:38:24 -0400

    add_to_any upgrade

--- wp-content/plugins/add-to-any/addtoany.widget.php
+++ wp-content/plugins/add-to-any/addtoany.widget.php
@@ -1,17 +1,21 @@
 <?php
+
+/**
+ * AddToAny WordPress Widget
+ */
 	
 class A2A_SHARE_SAVE_Widget extends WP_Widget {
 	/** constructor */
-    function A2A_SHARE_SAVE_Widget() {
-        parent::WP_Widget('', 'AddToAny Sharing', array('description' => 'Help people share, bookmark, and email your posts & pages using any service, such as Facebook, Twitter, StumbleUpon, Digg and many more.'), array('width' => 400));	
-    }
+	function A2A_SHARE_SAVE_Widget() {
+		parent::WP_Widget('', 'AddToAny Sharing', array('description' => 'Help people share, bookmark, and email your posts & pages using any service, such as Facebook, Twitter, StumbleUpon, Digg and many more.'), array('width' => 400));	
+	}
 	
 	/** Backwards compatibility for A2A_SHARE_SAVE_Widget::display(); usage */
 	function display( $args = false ) {
 		self::widget($args, NULL);
 	}
 
-    /** @see WP_Widget::widget */	
+	/** @see WP_Widget::widget */	
 	function widget($args = array(), $instance) {
 	
 		global $A2A_SHARE_SAVE_plugin_url_path;
@@ -41,25 +45,24 @@ class A2A_SHARE_SAVE_Widget extends WP_Widget {
 	}
 	
 	/** @see WP_Widget::update */
-    function update($new_instance, $old_instance) {				
+	function update($new_instance, $old_instance) {
 		$instance = $old_instance;
 		$instance['title'] = strip_tags($new_instance['title']);
 		return $instance;
-    }
+	}
 	
 	/** @see WP_Widget::form */
-    function form($instance) {
-    	$title = (isset($instance) && isset($instance['title'])) ? esc_attr($instance['title']) : '';
+	function form($instance) {
+		$title = (isset($instance) && isset($instance['title'])) ? esc_attr($instance['title']) : '';
 		?>
 		<p>
 			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
 			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
 		</p>
 		<p>
-	    	<a href="options-general.php?page=add-to-any.php"><?php _e("Settings", "add-to-any"); ?>...</a>
+			<a href="options-general.php?page=add-to-any.php"><?php _e("AddToAny Settings", "add-to-any"); ?>...</a>
 		</p>
 		<?php
-    }
+	}
 	
 }
-
