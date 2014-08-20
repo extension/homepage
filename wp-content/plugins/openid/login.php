431a2b5dd8fb22bafdc220f8dc530ab718e5ed6c
Jason Adam Young <jayoung@extension.org>
2014-08-20 13:54:02 -0400

    Another change from the original 3.3.3 diff

--- wp-content/plugins/openid/login.php
+++ wp-content/plugins/openid/login.php
@@ -21,7 +21,7 @@ add_action( 'init', 'openid_login_errors' );
  * @param mixed $user authenticated user object, or WP_Error or null
  */
 function openid_authenticate($user) {
-	if ( array_key_exists('openid_identifier', $_POST) && $_POST['openid_identifier'] ) {
+	if ( !array_key_exists('finish_openid', $_REQUEST) && $_GET['loggedout']!='true' ) {
 
 		$redirect_to = array_key_exists('redirect_to', $_REQUEST) ? $_REQUEST['redirect_to'] : null;
 		#openid_start_login($_POST['openid_identifier'], 'login', $redirect_to);
