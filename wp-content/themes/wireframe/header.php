25534fcdaad8d20fb365fd1722e6ab5377cceb9a
Ben MacNeill <ben.macneill@gmail.com>
2015-07-06 15:25:42 -0400

    remove blog description for now

--- wp-content/themes/wireframe/header.php
+++ wp-content/themes/wireframe/header.php
@@ -36,14 +36,18 @@
 						<div class="site-branding">
 							<?php
 								if ( is_front_page() && is_home() ) : ?>
-									<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+									<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+										<?php bloginfo( 'name' ); ?></a>
+									</h1>
 								<?php else : ?>
-									<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+									<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+										<?php bloginfo( 'name' ); ?></a>
+									</h1>
 								<?php endif;
 
 								$description = get_bloginfo( 'description', 'display' );
 								if ( $description || is_customize_preview() ) : ?>
-									<p class="site-description"><?php echo $description; ?></p>
+									<!-- <p class="site-description"><?php echo $description; ?></p> -->
 								<?php endif;
 							?>
 
