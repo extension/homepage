c07e511c1978e93f38fa0d7e07d8d5b88b72a8a8
Ben MacNeill <ben.macneill@gmail.com>
2015-07-27 18:49:00 -0400

    Ask and Learn wordsmithing

--- wp-content/themes/pinboard-child/ex-ask-module.php
+++ wp-content/themes/pinboard-child/ex-ask-module.php
@@ -1,8 +1,10 @@
 <aside class="widget widget_text">
   <div class="textwidget">
-    <h2>Ask an Expert</h2>
-    <p class="hp-resource-item"><a href="https://ask.extension.org/">See recent questions</a></p>
-    <p class="hp-action-item"><a href="https://ask.extension.org/expert/home">Volunteer to answer questions</a></p>
-    <p class="hp-action-item"><a class="btn btn-default btn-lg" href="https://ask.extension.org/widgets/">Create a question widget</a></p>
+    <a href="https://ask.extension.org/">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Ask_An_Expert_logo_color.png" alt="<?php bloginfo( 'name' ); ?>" />
+    </a>
+    <p class="hp-resource-item"></p>
+    <p class="hp-action-item"><a class="btn btn-primary btn-default btn-lg" href="https://ask.extension.org/home/accept_questions">Enable question submission on your site</a></p>
+    <p class="hp-action-item"><a class="btn btn-primary btn-default btn-lg" href="https://ask.extension.org/widgets/">Display Ask an Expert answers on your site</a></p>
   </div>
 </aside>
--- wp-content/themes/pinboard-child/ex-learn-module.php
+++ wp-content/themes/pinboard-child/ex-learn-module.php
@@ -2,7 +2,7 @@
   <div class="textwidget">
     <h2>Learn Webinars</h2>
     <p class="hp-resource-item"><a href="https://learn.extension.org/">Take charge of your professional development with Learn.extension.org</a></p>
-    <p class="hp-action-item"><a class="btn btn-default btn-lg" href="https://learn.extension.org/events/new">Create a Learn webinar</a></p>
-    <p class="hp-action-item"><a class="btn btn-default btn-lg" href="https://learn.extension.org/widgets">Create a Learn widget</a></p>
+    <p class="hp-action-item"><a class="btn btn-default btn-primary btn-lg" href="https://learn.extension.org/events/new">Create a Learn webinar</a></p>
+    <p class="hp-action-item"><a class="btn btn-default btn-primary btn-lg" href="https://learn.extension.org/widgets">Display upcoming Learn webinars on your site</a></p>
   </div>
 </aside>
