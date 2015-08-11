d2277e4afa4ad1390027a3694401b84d5ab1f2cb
Ben MacNeill <ben.macneill@gmail.com>
2015-08-11 16:44:22 -0400

    reorder modules

--- wp-content/themes/pinboard-child/sidebar-modules.php
+++ wp-content/themes/pinboard-child/sidebar-modules.php
@@ -1,6 +1,6 @@
 <div id="sidebar-modules" class="widget-area" role="complementary">
-	<?php get_template_part( 'ex-twitter-module' ); ?>
-	<?php get_template_part( 'ex-articles-module' ); ?>
 	<?php get_template_part( 'ex-learn-module' ); ?>
+	<?php get_template_part( 'ex-twitter-module' ); ?>
 	<?php get_template_part( 'ex-ask-module' ); ?>
+	<?php get_template_part( 'ex-articles-module' ); ?>
 </div><!-- #sidebar-modules -->
