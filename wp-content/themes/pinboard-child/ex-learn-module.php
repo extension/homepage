17d4e287e9c569f61c7d0606069cf99a5bee3f2b
Ben MacNeill <ben.macneill@gmail.com>
2015-08-12 09:30:51 -0400

    Update learn module

--- wp-content/themes/pinboard-child/ex-learn-module.php
+++ wp-content/themes/pinboard-child/ex-learn-module.php
@@ -1,7 +1,25 @@
 <aside class="widget widget_text">
   <div class="textwidget">
     <h2>Learn Webinars</h2>
-    <p class="hp-resource-item"><a href="https://learn.extension.org/">Take charge of your professional development with Learn.extension.org</a></p>
+    <p class="hp-resource-item hp-resource-item-large"><a href="https://learn.extension.org/">Take charge of your professional development with Learn.extension.org</a></p>
+
+<!-- Learn -->
+<script type="text/javascript">
+(function() {
+  function async_load(){
+    var ex_learn = document.createElement('script'); ex_learn.type = 'text/javascript'; ex_learn.async = true;
+    ex_learn.src = "https://learn.extension.org/widgets/events.js?widget_key=exlw-2a2ea80c&limit=2&tags=front page";
+    var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ex_learn, s);
+  }
+  if (window.attachEvent)
+    window.attachEvent('onload', async_load);
+    else
+      window.addEventListener('load', async_load, false);
+    })();
+</script>
+<div id="exlw-2a2ea80c"></div>
+<!-- Learn -->
+
     <p class="hp-action-item"><a class="btn btn-default btn-primary btn-lg" href="https://learn.extension.org/events/new">Create a Learn webinar</a></p>
     <p class="hp-action-item"><a class="btn btn-default btn-primary btn-lg" href="https://learn.extension.org/widgets">Display upcoming Learn webinars on your site</a></p>
   </div>
