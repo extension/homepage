02c8009592f3acc0bce1c007eebbefddffb45f2c
Ben MacNeill <ben.macneill@gmail.com>
2015-11-18 08:03:37 -0500

    incorporate Google custom search engine

---
+++ wp-content/themes/pinboard-child/search.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+	<div id="container" class="page">
+		<section id="search" <?php pinboard_content_class(); ?>>
+			<h1>Search results</h1>
+
+
+
+			<script>
+  (function() {
+    var cx = '002594610894210374936:hggphannnzi';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search queryParameterName="s" enableAutoComplete="true"></gcse:search>
+
+
+		</section><!-- #content -->
+		<?php if( ( 'no-sidebars' != pinboard_get_option( 'layout' ) ) && ( 'full-width' != pinboard_get_option( 'layout' ) ) ) : ?>
+			<?php get_sidebar(); ?>
+		<?php endif; ?>
+		<div class="clear"></div>
+	</div><!-- #container -->
+<?php get_footer(); ?>
