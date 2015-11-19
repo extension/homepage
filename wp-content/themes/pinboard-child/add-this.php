4955a499618bfe42c602980a64547db9bc92b655
Ben MacNeill <ben.macneill@gmail.com>
2015-11-19 10:04:06 -0500

    clean-up and clarification

--- wp-content/themes/pinboard-child/add-this.php
+++ wp-content/themes/pinboard-child/add-this.php
@@ -3,5 +3,8 @@ NOTE: Go to www.addthis.com/dashboard to customize the display of this widget
 -->
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51d4654b1c82f32e" async="async"></script>
 
+<div class="social-bookmarks">
+<p>Did you like this article? Share it with your friends!</p>
 <div class="addthis_native_toolbox large-screen"></div>
 <div class="addthis_sharing_toolbox small-screen"></div>
+</div>
