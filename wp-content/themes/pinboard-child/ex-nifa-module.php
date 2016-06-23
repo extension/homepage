e2ddf75dd3480dd0fcfe0f5b91cb7163785593fd
Ben MacNeill <ben.macneill@gmail.com>
2016-06-23 17:22:43 -0400

    Create NIFA / USDA module and add to all sidebars

---
+++ wp-content/themes/pinboard-child/ex-nifa-module.php
@@ -0,0 +1,6 @@
+<aside class="widget widget_text">
+  <div class="textwidget usda-nifa">
+    <p><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/usda-nifa.png" alt="<?php bloginfo( 'name' ); ?>" /></p>
+    <p>This work is supported by the USDA National Institute of Food and Agriculture, New Technologies for Ag Extension project.</p>
+  </div>
+</aside>
