c331b86d3bb92b8cd080b18c53c1af55c6b02a69
Ben MacNeill <ben.macneill@gmail.com>
2017-03-17 16:44:46 -0400

    Search template for WP content only

--- wp-content/themes/pinboard-child/search.php
+++ wp-content/themes/pinboard-child/search.php
@@ -1,29 +1,38 @@
 <?php get_header(); ?>
-	<div id="container" class="page search-container">
-		<section id="content" <?php pinboard_content_class(); ?>>
-			<h1>Search results</h1>
+<?php global $wp_query; ?>
+<div id="container" class="page">
+  <section id="content" <?php pinboard_content_class(); ?>>
+
+    <div class="search-page">
+        <h1 class="search-title"> <?php echo $wp_query->found_posts; ?>
+          <?php _e( 'Search Results Found For', 'locale' ); ?>: "<?php the_search_query(); ?>" </h1>
+
+          <p class="search-prompt"><span>You're searching eXtension Foundation news and updates. To search educational content, please visit <a href="http://articles.extension.org/">articles.eXtension.org</a>.</span></p>
+
+          <?php if ( have_posts() ) { ?>
+
+              <?php while ( have_posts() ) { the_post(); ?>
+
+                 <div class="search-list-item">
+                   <h3 class="search-header"><a href="<?php echo get_permalink(); ?>"><?php the_title();  ?></a></h3>
+                   <?php  the_post_thumbnail('medium') ?>
+                   <p><?php echo get_the_excerpt(); ?></p>
+                 </div>
+
+              <?php } ?>
+
+             <?php paginate_links(); ?>
+
+          <?php } ?>
+
+      </div>
+
+  </section><!-- #content -->
+  <?php if( ( 'no-sidebars' != pinboard_get_option( 'layout' ) ) && ( 'full-width' != pinboard_get_option( 'layout' ) ) ) : ?>
+    <?php get_sidebar(); ?>
+  <?php endif; ?>
+  <div class="clear"></div>
+</div><!-- #container -->
 
 
-
-			<script>
-  (function() {
-    var cx = '002594610894210374936:hggphannnzi';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-        '//cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  })();
-</script>
-<gcse:search queryParameterName="s" enableAutoComplete="true"></gcse:search>
-
-
-		</section><!-- #content -->
-		<?php if( ( 'no-sidebars' != pinboard_get_option( 'layout' ) ) && ( 'full-width' != pinboard_get_option( 'layout' ) ) ) : ?>
-			<?php get_sidebar(); ?>
-		<?php endif; ?>
-		<div class="clear"></div>
-	</div><!-- #container -->
 <?php get_footer(); ?>
