06afce5ca327e7a043e2cf9e3cebb39dc8d7c27c
Ben MacNeill <ben.macneill@gmail.com>
2017-05-24 12:01:42 -0400

    Remove sidebar content module

--- wp-content/themes/pinboard-child/sidebar-modules.php
+++ wp-content/themes/pinboard-child/sidebar-modules.php
@@ -1,5 +1,4 @@
 <div id="sidebar-modules" class="widget-area" role="complementary">
-	<?php get_template_part( 'ex-calls-to-action-module' ); ?>
 	<?php get_template_part( 'ex-learn-module' ); ?>
 	<?php get_template_part( 'ex-twitter-module' ); ?>
   <?php get_template_part( 'ex-nifa-module' ); ?>
