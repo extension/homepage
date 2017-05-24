64bd595db6f1571e110a606e2b595d76e774348e
Ben MacNeill <ben.macneill@gmail.com>
2017-05-24 15:52:47 -0400

    Add Cooperative Extension logo to sidebar

--- wp-content/themes/pinboard-child/ex-nifa-module.php
+++ wp-content/themes/pinboard-child/ex-nifa-module.php
@@ -1,6 +1,7 @@
 <aside class="widget widget_text">
   <div class="textwidget usda-nifa">
     <p><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/usda-nifa.png" alt="<?php bloginfo( 'name' ); ?>" /></p>
+    <p class="sidebar-image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cooperative_extension.png" alt="<?php bloginfo( 'name' ); ?>" /></p>
     <p>This work is supported by the USDA National Institute of Food and Agriculture, New Technologies for Ag Extension project.</p>
   </div>
 </aside>
