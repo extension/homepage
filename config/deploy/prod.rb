66262e36242aeebd74b22fe82f67b238390348fb
Jason Adam Young <jayoung@extension.org>
2017-06-13 11:09:37 -0400

    deployment target

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,5 +1,5 @@
 set :deploy_to, "/services/homepage/"
 set :branch, 'master'
-set :vhost, 'extension.org'
+set :vhost, 'www.extension.org'
 set :deploy_server, 'homepage.aws.extension.org'
 server deploy_server, :app, :web, :db, :primary => true
